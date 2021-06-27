fbb6d8aa26c389fcbd6bdda91fb6455b2be2b55d
Hann1234 <christopherjhannigan@gmail.com>
2021-06-27 17:18:42 -0500

    defined package to be sent over server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,22 +2,39 @@ $(document).ready(onReady);
 console.log('JS');
 
 let operator = '';
+let package = {
+    packageNum1: 0,
+    packageNum2: 0,
+    packageOperator: ''
+};
 
-function clearInputs(){
+function clearInputs(){ //clears inputs
     $('#firstNumber').val('');
     $('#secondNumber').val('');
   }
 
-function onReady() {
+  function handleOperatorClick() { //defines what operator is being selected
+    operator = $(this).text();
+    console.log(operator);
+}
+
+function equalClick() {
+if ($('#firstNumber').val() === '' || $('#secondNumber').val() === '' || operator === '') {
+    alert('Please enter two numbers and click an operator');
+} else {
+    package.packageNum1 = $('#firstNumber').val();
+    package.packageNum2 = $('#secondNumber').val();
+    package.packageOperator = operator;
+}
+
+
+}
+
+function onReady() { //runs when page loads and sets up listeners ready for some clickies
   console.log('JQ');
   clearInputs();
-//   $('body').on('click', "button.operatorButton", () => console.log($(this).text()));
-  $('#equalButton').on('click', () => console.log('equal'));
+  $('#equalButton').on('click', equalClick);
   $('#clearButton').on('click', () => clearInputs());
   $('.operatorButton').on('click', handleOperatorClick);
 }
 
-function handleOperatorClick() {
-    operator = $(this).text();
-    console.log(operator);
-}
