7ddda1e44b3ada8b1a19c75940bd2e6f3602d822
Hann1234 <christopherjhannigan@gmail.com>
2021-06-27 13:16:32 -0500

    created server

---
+++ server/server.js
@@ -0,0 +1,13 @@
+// configuring server
+const express = require('express');
+const app = express();
+const port = 5000; 
+
+app.use(express.static('server/public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log('listening on port', port);
+  });
+  
