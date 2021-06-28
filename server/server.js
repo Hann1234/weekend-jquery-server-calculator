427c2175b0ceec28c45df9d3e205c30e3b891f36
Hann1234 <christopherjhannigan@gmail.com>
2021-06-28 09:45:01 -0500

    working along with Edan to fix mistakes

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = 5000; 
 
-app.use(express.static('server/public'));
+app.use(express.static('./server/public')); //servs matching file to send back html
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
