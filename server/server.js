68931a0f70ebeb3f613e278e160d42018837fb4e
Hann1234 <christopherjhannigan@gmail.com>
2021-06-27 21:46:59 -0500

    added calculator function to server side

--- server/server.js
+++ server/server.js
@@ -10,4 +10,32 @@ app.use(express.json());
 app.listen(port, () => {
     console.log('listening on port', port);
   });
-  
+
+let answer = 0;
+
+let package = {
+    packageNum1: 0,
+    packageNum2: 0,
+    packageOperator: ''
+};
+
+
+//Calculator function
+function solveEquation(package) {
+    switch (package.packageOperator) {
+        case '+':
+            answer = Number(package.packageNum1) + Number(package.packageNum2); 
+            break;
+        case '-':
+            answer = Number(package.packageNum1) - Number(package.packageNum2); 
+            break;
+        case '*':
+            answer = Number(package.packageNum1) * Number(package.packageNum2); 
+            break;
+        case '/':
+            answer = Number(package.packageNum1) / Number(package.packageNum2); 
+            break;
+        default:
+            break;
+    }
+}
